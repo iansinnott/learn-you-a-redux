f728942444c37044d42df7cb18746718ecf6f551
Ian Sinnott <ian@iansinnott.com>
2017-05-13 16:09:26 -0700

    Initialize app

---
+++ README.md
@@ -0,0 +1,31 @@
+# Learn You a Redux
+
+I feel that I already have a pretty good grasp of Redux, but I figured I'd build it myself to get a sense of what's involved. Reading through [this article][redux article] inspired me.
+
+[redux article]: https://zapier.com/engineering/how-to-build-redux/
+
+**Quick Start:**
+
+* `npm install`
+* `npm start` to run a dev server
+* `npm run build` to minify, package and generate static HTML files from your routes
+
+
+## Troubleshooting
+
+### Babel Env
+
+ Make sure `BABEL_ENV` is not set to `development` when building. If it is babel will likely throw a hot module replacement error since HMR transformations are getting run on everything that goes through babel.
+
+[React]: http://facebook.github.io/react/
+[Webpack]: https://webpack.js.org/
+[Babel]: https://babeljs.io/
+[Stylus]: https://learnboost.github.io/stylus/
+[CSS Modules]: https://github.com/css-modules/css-modules
+[css-loader-modules]: https://github.com/webpack/css-loader#css-modules
+[Express]: http://expressjs.com/
+[Waterline]: https://github.com/balderdashy/waterline
+[Flux]: https://facebook.github.io/flux/docs/overview.html
+[React Router]: https://github.com/rackt/react-router
+[Redux]: https://github.com/rackt/redux
+[Docker Compose]: https://docs.docker.com/compose/
---
+++ client/components/App.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import T from 'prop-types';
+import { Link, IndexLink } from 'react-router';
+import classnames from 'classnames/bind';
+
+// Using CSS Modules so we assign the styles to a variable
+import s from './App.styl';
+const cx = classnames.bind(s);
+import logo from './react-logo.png';
+
+// Favicon link is in the template, this just makes webpack package it up for us
+import './favicon.ico';
+
+export class Home extends React.Component {
+  render() {
+    return (
+      <div className={cx('page')}>
+        <div className={cx('siteTitle')}>
+          <h1>Learn you a redux</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export class About extends React.Component {
+  render() {
+    return (
+      <div className={cx('page')}>
+        <div className={cx('siteTitle')}>
+          <h1>About Page</h1>
+        </div>
+        <p>Welcome to the about page...</p>
+      </div>
+    );
+  }
+}
+
+export class NotFound extends React.Component {
+  render() {
+    return (
+      <div className={cx('page')}>
+        <h4>Not found</h4>
+      </div>
+    );
+  }
+}
+
+/**
+ * NOTE: As of 2015-11-09 react-transform does not support a functional
+ * component as the base compoenent that's passed to ReactDOM.render, so we
+ * still use createClass here.
+ */
+export class App extends React.Component {
+  static propTypes = {
+    children: T.node,
+  };
+
+  render() {
+    return (
+      <div className={cx('App')}>
+        <nav className={cx('nav')}>
+          <IndexLink to='/' activeClassName={cx('active')}>Home</IndexLink>
+          <Link to='/about' activeClassName={cx('active')}>About</Link>
+        </nav>
+        {this.props.children}
+      </div>
+    );
+  }
+}
